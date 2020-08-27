cd7bcca90e60d80623a4691d445d118db9383ca9
Derek Zhang <leizhang0121@gmail.com>
2020-08-28 00:29:34 +0800

    updated hitun subscription link

--- provider/hitun.js
+++ provider/hitun.js
@@ -2,13 +2,13 @@
 const { utils } = require('surgio');
 
 module.exports = {
-  url: 'https://rss-node.com/link/qI2KKJqHVBRyBHSL?mu=1',
+  url: 'https://rss-node.com/link/FvKOEaQUb70Bq1Jh?mu=1',
   type: 'shadowsocksr_subscribe',
   // 定义所有的节点都支持 udpRelay
   udpRelay: true,
   // 添加国旗 emoji
   addFlag: true,
-  nodeFilter: utils.discardKeywords(['地址','流量','时间','Free','续费','官网','回国','用户','设定']),
+  nodeFilter: utils.discardKeywords(['地址','流量','时间','Free','续费','官网','回国','用户','设定','限速', '低速']),
   relayUrl: true,
   tfo: true
 };
