f31131e6ce604f0f775cec29968f06855cee94f8
Derek Zen <lei.zhang@stardust.ai>
2024-11-11 18:12:11 +0800

    updated index.js for netlify

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -2,4 +2,16 @@
 
 const gateway = require('@surgio/gateway');
 
-module.exports.handler = gateway.createLambdaHandler();
+module.exports.handler = gateway.createLambdaHandler({
+    // Required configuration
+    config: {
+        urlBase: process.env.SURGIO_URL_BASE || 'http://localhost:3000',
+        artifacts: [],
+        providers: []
+    },
+    // Optional but recommended
+    // auth: {
+    //     username: process.env.SURGIO_AUTH_USERNAME,
+    //     password: process.env.SURGIO_AUTH_PASSWORD
+    // }
+});
