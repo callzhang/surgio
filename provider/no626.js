0182ca506990d8d6f0952f1b91cdea178b80072e
gary <gary@garydeMacBook-Pro.local>
2026-03-16 14:47:14 +0800

    update ne26 describe url

--- provider/no626.js
+++ provider/no626.js
@@ -2,15 +2,16 @@
 const { utils } = require('surgio');
 // https://www.no626.online/
 module.exports = {
-  url: 'https://ul8knn76p9j0.no626.link/api/v1/client/subscribe?token=e149d70fc20d1610bae5d2000dae237b',
-  type: 'clash',
-  // 定义所有的节点都支持 udpRelay
-  udpRelay: true,
-  // 添加国旗 emoji
-  addFlag: true,
-  nodeFilter: utils.discardKeywords(['官网','回国','用户','设定','限速', '游戏','低速']),
-  tfo: true,
-  renameNode: name => {
-    return name + ' | no626';
-  },
+    url: 'https://subscription.no626.link/NO626/system/api/v1/client/subscribe?token=e149d70fc20d1610bae5d2000dae237b',
+    //   url: 'https://ul8knn76p9j0.no626.link/api/v1/client/subscribe?token=e149d70fc20d1610bae5d2000dae237b',
+    type: 'clash',
+    // 定义所有的节点都支持 udpRelay
+    udpRelay: true,
+    // 添加国旗 emoji
+    addFlag: true,
+    nodeFilter: utils.discardKeywords(['官网', '回国', '用户', '设定', '限速', '游戏', '低速']),
+    tfo: true,
+    renameNode: name => {
+        return name + ' | no626';
+    },
 };
